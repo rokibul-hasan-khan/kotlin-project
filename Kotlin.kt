4a0d12136c0b2b7a698333ffa6bf1de9b39f5f62
rokibul-hasan-khan <hrokibul64@gmail.com>
2022-03-01 20:32:50 +0600

    kotlin file commiter

---
+++ Kotlin.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val names = arrayOf("John","Jeny", "Jemmy", "Jerry")
+    names.indices
+    println("Index: ${names} Element: ")
+
+    //for (name in names)  println(name)
+
+    //for (i in 'a'..'z' step 2) println(i)
+    //for (i in 1 until 10) println(i)
+   // for (i in 10L downTo  1) println(i)
+   // for (i in 1.rangeTo(10)) println(i)
+
+
+
+
+}
---
+++ okkk.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var userName: String = "Florian"
+    var age: Int = 28
+    println("Hello World $userName. I am $age")
+
+}
+
